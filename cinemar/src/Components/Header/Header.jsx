cecf84e459d6d609f3fef52f25036950cc46941a
Reimar-Coelho <reimargamer@proton.me>
2024-05-06 12:51:18 -0300

    criação dos componentes iniciais

---
+++ cinemar/src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../Navbar/Navbar";
+
+function Header() {
+    return (
+        <>
+            <header>
+                <span>Olá, visitante!</span>
+                <h1> Bem-Vindo ao CineMar</h1>
+                <Navbar/>
+            </header>
+        </>
+    );
+}
+
+export default Header;
