6b70b063bdb90eab28ca5d6bb8742835b75855e3
Reimar-Coelho <reimargamer@proton.me>
2024-05-06 12:59:51 -0300

    Adição router dom

--- cinemar/src/App.jsx
+++ cinemar/src/App.jsx
@@ -1,9 +1,13 @@
+import Header from "./Components/Header/Header"
+import {Outlet} from "react-router-dom"
+
 function App() {
 
 
   return (
     <>
-      <h1> App</h1>
+      <Header/>
+      <Outlet/>
     </>
   )
 }
