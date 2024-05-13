45949d4fea756939b5e5fa8b7b953d0f2e2e70f8
Reimar-Coelho <reimargamer@proton.me>
2024-05-13 12:59:53 -0300

    adição tailwind e configuração de rotas

--- cinemar/src/Components/Header/Header.jsx
+++ cinemar/src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import Navbar from "../Navbar/Navbar";
 function Header() {
     return (
         <>
-            <header>
+            <header className="flex">
                 <span>Olá, visitante!</span>
                 <h1> Bem-Vindo ao CineMar</h1>
                 <Navbar/>
