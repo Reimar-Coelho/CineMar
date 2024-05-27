e71aaacf9af699dbae31147c8e035b359763dd19
Reimar-Coelho <reimargamer@proton.me>
2024-05-27 12:53:16 -0300

    imagens e cards

--- cinemar/src/Pages/Home.jsx
+++ cinemar/src/Pages/Home.jsx
@@ -7,13 +7,33 @@ function Home() {
             <main>
                 <input type="text" name="busca" id="busca" placeholder="Digite aqui sua busca"/>
                 <div className="lista-filmes">
+                    <div className='grid grid-cols-3 gap-4'>
                     {
                         data.map(
                             filme => (
-                                <h1>{filme.title}</h1>
+                                <div className="card border-2" key={filme.title}>
+                                    <h1>{filme.title}</h1>
+                                    <img className='w-80' src={filme.image} alt={filme.image} />
+                                    <div className="tag">
+                                        {filme.tags.map(
+                                            tag => (
+                                            <span key={tag}>{tag}</span>
+                                            )
+                                        )}
+                                    </div>
+                                    <div className="text">
+                                        {filme.text.map(
+                                            text => (
+                                                <span key={text}>{text}</span>
+                                            )
+                                        )}
+                                    </div>
+                                </div>
                             )
                         )
                     }
+                    </div>
+                    
                 </div>
             </main>
         </>
