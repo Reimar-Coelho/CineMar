722f9c31b3362e6a6be205fab99fa0305993f7ce
Reimar <reimargamer@proton.me>
2024-06-10 12:59:15 -0300

    finalização backend filmes

--- cinemar/src/Pages/Filmes.jsx
+++ cinemar/src/Pages/Filmes.jsx
@@ -1,33 +1,41 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import {Link} from 'react-router-dom'
 
 function Filmes() {
 
     const [filmes, setFilmes] = useState([])
 
-    const apiKey='d1bef122d48992b960cc79c7306f5c1b'
+    const apiKey='api_key=d1bef122d48992b960cc79c7306f5c1b'
     const urlBase = 'https://api.themoviedb.org/3/movie/'
     const urlImg = 'https://image.tmdb.org/t/p/w342/'
 
     
+    useEffect( () => {
+        fetch(`${urlBase}popular?${apiKey}`)
+        .then(response => response.json())
+        .then(response => setFilmes(response.results))
+        .catch(erro => console.log(erro))
+    },[])
     
-    fetch('https://api.themoviedb.org/3/movie/popular?api_key=d1bef122d48992b960cc79c7306f5c1b')
-    .then(response => response.json())
-    .then(response => setFilmes(response.results))
-    .catch(erro => console.log(erro))
 
-    console.log(filmes, setFilmes, apiKey, urlBase, urlImg)
+    
     
     return (
         
         <>
             <h1>Filmes</h1>
+            <div className="listaFilmes flex flex-row">
             {
                 filmes.map(filme => (
                     <div className="card-filme" key={filme.id}>
+                        <img src={`${urlImg}${filme.poster_path}`}/>
                         <h1>{filme.title}</h1>
+                        <Link to={`filmes/:${filme.id}`} className="bg-blue-500">Saber mais</Link>
                     </div>
                 ))
             }
+            </div>
+            
         </>
     );
     
